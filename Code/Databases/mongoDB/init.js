dd98a0cba8589b15f660414eac0e574a643bdfca
josue-balbontin <balbontinjosue@gmail.com>
2026-06-22 23:20:59 -0400

    subiendo backup con datos

--- Code/Databases/mongoDB/init.js
+++ Code/Databases/mongoDB/init.js
@@ -18,12 +18,36 @@ db.createCollection('resenas_productos');
 
 db.resenas_productos.insertMany([
   {
-    id_producto: 1,
-    id_usuario_cliente: 2,
-    calificacion: 5,
-    comentario: "Excelente producto, inicialización exitosa.",
-    fecha_creacion: new Date(),
-    util_votos: 0
+    "id_producto": 1,
+    "id_usuario_cliente": 2,
+    "calificacion": 5,
+    "comentario": "Excelente producto, inicialización exitosa.",
+    "fecha_creacion": new Date(),
+    "util_votos": 0
+  },
+  {
+    "id_producto": 1,
+    "id_usuario_cliente": 5,
+    "calificacion": 5,
+    "comentario": "Excelente laptop para programar, levanta Docker y Visual Studio sin problemas.",
+    "fecha_creacion": new Date(),
+    "util_votos": 12
+  },
+  {
+    "id_producto": 3,
+    "id_usuario_cliente": 5,
+    "calificacion": 4,
+    "comentario": "Son cómodas para correr, pero la talla es un poco justa. Recomiendo pedir un número más.",
+    "fecha_creacion": new Date(),
+    "util_votos": 3
+  },
+  {
+    "id_producto": 1,
+    "id_usuario_cliente": 99,
+    "calificacion": 5,
+    "comentario": "El envío fue rapidísimo y el producto llegó impecable.",
+    "fecha_creacion": new Date(new Date().setDate(new Date().getDate() - 5)), // Hace 5 días
+    "util_votos": 8
   }
 ]);
 
